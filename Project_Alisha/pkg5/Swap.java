65870ddf5f071f6a05958c026858c8db28363cac
Alisha.Chawla <Alisha.Chawla@IND00139.emea.akqa.local>
2020-02-16 20:44:29 +0530

    New Commit

---
+++ Project_Alisha/pkg5/Swap.java
@@ -0,0 +1,29 @@
+package pkg5;
+
+import java.util.Scanner;
+
+public class Swap {
+public void swap (int a, int b)
+	
+	{
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+	System.out.println("value of a is " +a);	
+	System.out.println("value of b is " +b);	
+	}
+	
+	public static void main(String[] args)
+	{
+		SwapV obj= new SwapV();
+		Scanner s= new Scanner (System.in);
+		System.out.println("Enter value of a=");
+		int a= s.nextInt();
+		System.out.println("Enter value of b=");
+		int b= s.nextInt();
+		obj.swap(a, b);
+		
+	}
+	
+}
